7b9733068fdee50b21e7d213f99a9b65806e52b6
rssathishsundarraj <rssathishsundarraj@gmail.com>
2026-04-14 20:15:58 +0530

    Initial Commit

---
+++ VoiceChat/VoiceChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceChatApp.swift
+//  VoiceChat
+//
+//  Created by Sathish Kumar RS on 14/04/26.
+//
+
+import SwiftUI
+
+@main
+struct VoiceChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
